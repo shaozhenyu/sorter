bfb93416b6a00a930154af042e2e3490a557d464
shaozhenyu <18217258834@163.com>
2016-04-05 23:50:31 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,16 @@
 #!bin/bash
-go build algorithms/qsort
-go test algorithms/qsort
 
-go build algorithms/bubblesort
-go test algorithms/bubblesort
+filelist=`ls src/algorithms`
+for file in $filelist
+do
+	go build algorithms/$file
+	go test algorithms/$file
+done
 
-go build algorithms/mergesort
-go test algorithms/mergesort
-
-go build algorithms/heapsort
-go test algorithms/heapsort
-
-go install algorithms/qsort
-go install algorithms/bubblesort
-go install algorithms/mergesort
-go install algorithms/heapsort
+for file in $filelist
+do
+	go install algorithms/$file
+done
 
 go build sorter
 go install sorter
